22540a107c823356a8ef7f43241a412b17344472
devayush <devayushbajaj@gmail.com>
2024-10-24 00:19:49 +0530

    Added boiler plate files

---
+++ src/main/java/com/dev/multiThreadingUsingSpringBootDemo/MultiThreadingUsingSpringBootDemoApplication.java
@@ -0,0 +1,13 @@
+package com.dev.multiThreadingUsingSpringBootDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiThreadingUsingSpringBootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiThreadingUsingSpringBootDemoApplication.class, args);
+	}
+
+}
